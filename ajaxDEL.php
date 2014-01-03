c7c5369e35235b082b6861489e62e8b468155a88
s4l1h <kingemkurdam@gmail.com>
2014-01-03 22:11:23 +0200

    Security fix.

--- ajaxDEL.php
+++ ajaxDEL.php
@@ -1,5 +1,13 @@
 <?php
-include 'models/chat.config.php';
+require_once('models/config.php');
+require_once('models/chat.config.php');
+
+$user_id = $loggedInUser->user_id;
+if(isUserAdmin($id)===FALSE && isUserMod($id)===FALSE){
+    exit(':)');
+}
+
+
 
 $idz = $db->real_escape_string($_POST['id']);
 $query = $db->Query("UPDATE messages SET `hidden`='1' WHERE `id`='$idz'");
